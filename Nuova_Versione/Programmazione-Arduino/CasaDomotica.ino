e369fe8b6002b04707797a5352a01fa9ea7f4ee8
MattiaFerraioli <ferra97tia@gmail.com>
2016-05-12 13:03:09 +0200

    Versione 1
    Incompleto

---
+++ Nuova_Versione/Programmazione-Arduino/CasaDomotica.ino
@@ -0,0 +1,36 @@
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(13, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  String val= "SLED";
+  
+
+  if(val=="ALED")
+  {
+    digitalWrite(13, HIGH);
+    Serial.println("LedAccesso");
+    val= "SLED";
+    
+  }
+  else if(val=="SLED")
+  {
+    digitalWrite(13, HIGH);
+    Serial.println("LedSpento");
+  }
+  else if(val=="TEMP")
+  {
+    exit;
+  }
+
+
+  
+}
+
+
+
